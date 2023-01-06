2d50b5ed4b91085856f8c423f9150c8eafeb32b9
whippo <whippocode@gmail.com>
2023-01-06 15:35:35 -0300

    first commit

--- src/controllers/news.controllers.js
+++ src/controllers/news.controllers.js
@@ -1,16 +1,7 @@
 import News from "../models/news";
-import cloudinary from "cloudinary";
 import * as dotenv from "dotenv";
-import fs from "fs-extra";
-import alert from "alert";
 
 dotenv.config();
-
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
 
 export const renderIdex = async (req, res) => {
   const news = await News.find().lean();
@@ -20,30 +11,18 @@ export const renderIdex = async (req, res) => {
 export const apiGradual = async (req, res) => {
   const news = await News.find();
 
-  res.send({status: "ok", results: news});
+  res.send({ status: "ok", results: news });
 };
 
 export const newsSave = async (req, res) => {
-  if (req.file.size < 10000000) {
-    const photo = await cloudinary.v2.uploader.upload(req.file.path);
+  const newNews = new News({
+    title: req.body.title,
+    description: req.body.description,
+    content: req.body.content,
+  });
+  await newNews.save();
 
-    const newNews = new News({
-      image_url: photo.url,
-      image_id: photo.public_id,
-      category: req.body.category,
-      title: req.body.title,
-      description: req.body.description,
-      content: req.body.content,
-      Date: photo.created_at,
-    });
-    await newNews.save();
-
-    res.redirect("/");
-  } else {
-    alert("El archivo excede el tamaño permitido");
-  }
-
-  await fs.unlink(req.file.path);
+  res.redirect("/");
 };
 
 export const newsDelete = async (req, res) => {
--- src/models/news.js
+++ src/models/news.js
@@ -2,19 +2,6 @@ import { Schema, model } from "mongoose";
 
 const newsSchema = new Schema(
   {
-    photo: {
-      type: String,
-    },
-    image_url: {
-      type: String,
-    },
-    image_id: {
-      type: String,
-    },
-    category: {
-      type: String,
-      trim: true,
-    },
     title: {
       type: String,
       trim: true,

58abd5b9edbacc8928938a01ad64da64eefbf018
Whippo <whippocode@gmail.com>
2023-02-07 01:33:59 -0300

    update vercel.json4

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import app from "./src/app";
-import { mongoConnect } from "./src/database/databs";
+import app from "./app";
+import { mongoConnect } from "./database/databs";
 
 const port = process.env.PORT || 4000;
 
